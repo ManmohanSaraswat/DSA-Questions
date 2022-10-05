071bb2f6c519b933de3f790cac0d936fbc9ec905
Manmohan Saraswat <manmohansaraswat15@gmail.com>
2022-10-05 17:02:25 +0530

    Three approach added to 'add one row to tree' code

--- AddOneRowToTree.java
+++ AddOneRowToTree.java
@@ -68,4 +68,62 @@ public class AddOneRowToTree {
         }
         return root;
     }
+    public TreeNode addOneRowEfficient(TreeNode root, int val, int depth) {
+        if(depth == 1)
+            return new TreeNode(val, root, null);
+        if(depth == 2){
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+            return root;
+        }
+        depth --;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root); que.add(null);
+        while(que.isEmpty() == false){
+            TreeNode temp = que.poll();
+            if(temp == null && que.isEmpty() == false) {
+                que.add(null);
+                if(depth == 2){
+                    while(que.isEmpty() == false){
+                        TreeNode t = que.poll();
+                        if(t == null) break;
+                        if(t.left != null){
+                            t.left = new TreeNode(val, t.left, null);
+                        }else{
+                            t.left = new TreeNode(val);
+                        }
+                        if(t.right != null){
+                            t.right = new TreeNode(val, null, t.right);
+                        }else{
+                            t.right = new TreeNode(val);
+                        }
+                    }
+                    break;
+                }
+                depth --;
+                continue;
+            }
+            if(temp.left != null) {
+                que.add(temp.left);
+            }
+            if(temp.right != null) {
+                que.add(temp.right);
+            }
+        }
+        return root;
+    }
+    public TreeNode addOneRowMoreEfficient(TreeNode root, int val, int depth) {
+        if(depth == 1)
+            return new TreeNode(val, root, null);
+        if(depth == 2){
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+            return root;
+        }
+        if(root.left != null)
+            root.left = addOneRowMoreEfficient(root.left, val, depth - 1);
+        if(root.right != null)
+            root.right = addOneRowMoreEfficient(root.right, val, depth - 1);
+        return root;
+    }
 }
