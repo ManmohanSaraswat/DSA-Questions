efc3db5805910ac23cc8961530cf5684e344aafc
Manmohan Saraswat <manmohansaraswat15@gmail.com>
2022-10-14 10:42:43 +0530

    new code added

---
+++ DeleteTheMiddleOfALinkedList.java
@@ -0,0 +1,53 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class DeleteTheMiddleOfALinkedList {
+    public ListNode deleteMiddleApproach1(ListNode head) {
+        if (head.next == null)
+            return null;
+        int n = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            n++;
+        }
+        n = n / 2;
+        temp = head;
+        for (int i = 0; i < n - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        return head;
+    }
+
+    public ListNode deleteMiddleApproach2(ListNode head) {
+        if (head.next == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null) {
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else
+                fast = fast.next;
+            if (fast == null)
+                continue;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
