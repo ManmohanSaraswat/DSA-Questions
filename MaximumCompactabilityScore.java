3ef34bb7ff9f706c2578af78d713e432ca435b0d
Manmohan Saraswat <manmohansaraswat15@gmail.com>
2022-10-21 16:15:05 +0530

    file added

--- MaximumCompactabilityScore.java
+++ MaximumCompactabilityScore.java
@@ -1,34 +1,55 @@
+import java.util.Arrays;
+
 public class MaximumCompactabilityScore {
     int finalAns;
+    int[][] students;
+    int[][] mentors;
+    int[][] dp;
 
-    public void solve(int[][] students, int[][] mentors, int s, int[] assignedArr) {
+    public void solve(int s, int bit, int score) {
         if (s == students.length) {
-            int count = 0;
-            for (int i = 0; i < students.length; i++) {
-                int m = assignedArr[i];
-                for (int j = 0; j < mentors[m].length; j++) {
-                    if (students[i][j] == mentors[m][j])
-                        count++;
-                }
-            }
-            finalAns = Math.max(count, finalAns);
+            finalAns = Math.max(score, finalAns);
             return;
         }
         for (int i = 0; i < mentors.length; i++) {
-            int j = 0;
-            for (; j < s; j++)
-                if (assignedArr[j] == i)
-                    break;
-            if (j != s)
-                continue;
-            assignedArr[s] = i;
-            solve(students, mentors, s + 1, assignedArr);
+            if ((bit & (1 << i)) == 0)
+                solve(s + 1, (bit | (1 << i)), score + calculateScore(s, i));
         }
     }
 
-    public int maxCompatibilitySum(int[][] students, int[][] mentors) {
+    public int maxCompatibilitySum(int[][] s, int[][] m) {
+        students = s;
+        mentors = m;
         finalAns = Integer.MIN_VALUE;
-        solve(students, mentors, 0, new int[students.length]);
+        solve(0, 0, 0);
         return finalAns;
+    }
+
+    private int calculateScore(int student, int mentor) {
+        int size = students[student].length, score = 0;
+        for (int i = 0; i < size; i++) {
+            score += (students[student][i] == mentors[mentor][i]) ? 1 : 0;
+        }
+        return score;
+    }
+    public int solveMemoization(int s, int bit) {
+        if (s == students.length) {
+            return 0;
+        }
+        if(dp[s][bit] != -1) return dp[s][bit];
+        int ans = 0;
+        for (int i = 0; i < mentors.length; i++) {
+            if((bit & (1 << i)) == 0)
+                ans = Math.max(ans, calculateScore(s, i) + solveMemoization(s + 1, (bit | (1 << i))));
+        }
+        return dp[s][bit] = ans;
+    }
+    public int maxCompatibilitySumDpMemoizationBitMask(int[][] s, int[][] m) {
+        students = s;
+        mentors = m;
+        dp = new int[9][256];
+        for(int[] i: dp)
+            Arrays.fill(i, -1);
+        return solveMemoization(0, 0);
     }
 }
