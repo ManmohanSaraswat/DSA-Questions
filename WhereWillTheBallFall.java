02b2fe41b4a9ac65c8095c2fcd0452ce86f0a103
Manmohan Saraswat <40047862+ManmohanSaraswat@users.noreply.github.com>
2022-11-01 09:16:35 +0530

    code added

--- WhereWillTheBallFall.java
+++ WhereWillTheBallFall.java
@@ -23,4 +23,24 @@ public class WhereWillTheBallFall {
         }
         return arr;
     }
+
+    private int solveOptimized(int[][] grid, int i, int j, int n, int m) {
+        if (i == n) return j;
+        if (i < 0 || j < 0 || j == m) return -1;
+        if (grid[i][j] == 1) {
+            if (j + 1 == m || (j + 1 < m && grid[i][j + 1] == -1)) return -1;
+            return solveOptimized(grid, i + 1, j + 1, n, m);
+        } else {
+            if (j - 1 == -1 || (j - 1 >= 0 && grid[i][j - 1] == 1)) return -1;
+            return solveOptimized(grid, i + 1, j - 1, n, m);
+        }
+    }
+
+    public int[] findBallOptimized(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int[] res = new int[m];
+        for (int i = 0; i < m; i++)
+            res[i] = solveOptimized(grid, 0, i, n, m);
+        return res;
+    }
 }
