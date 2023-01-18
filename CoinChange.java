9d3c0e398d1888d3de1b49767b5160ed05940b26
Manmohan Saraswat <manmohansaraswat15@gmail.com>
2023-01-18 19:12:39 +0530

    code added

---
+++ CoinChange.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 1000000);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+            }
+        }
+        return dp[amount] >= 1000000 ? -1 : dp[amount];
+    }
+}
