818334cf4ca3b5feb0bba473484f80f73eba3262
Manmohan Saraswat <manmohansaraswat15@gmail.com>
2023-01-18 16:55:58 +0530

    code added

--- LengthofLongestFibonacciSubsequence.java
+++ LengthofLongestFibonacciSubsequence.java
@@ -1,6 +1,26 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LengthofLongestFibonacciSubsequence{
+    public int lenLongestFibSubseqEfficient(int[] arr) {
+        int res = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int i: arr) set.add(i);
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                int next = arr[i] + arr[j], temp = 0, prev = arr[j];
+                while(set.contains(next)){
+                    temp ++;
+                    int x = next;
+                    next = next + prev;
+                    prev = x;
+                }
+                res = Math.max(res, 2 + temp);
+            }
+        }
+        return res >= 3 ? res : 0;
+    }
     private int maxLen(int[] arr, int start, int next){
         int res = 0;
         while(true){
